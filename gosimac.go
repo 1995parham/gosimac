68ccd701974f52d5b069f230c0565e8befce17ad
Parham Alvani <parham.alvani@gmail.com>
2015-11-26 23:36:08 +0330

    GoSiMac relase 1.0 is ready.

--- gosimac.go
+++ gosimac.go
@@ -10,17 +10,21 @@
 package main
 
 import (
-	"./bing"
-	"os/user"
+	"flag"
 	"fmt"
+	"github.com/1995parham/gosimac/bing"
 	"github.com/golang/glog"
+	"os/user"
 )
 
 func main() {
+	var num int
+	flag.IntVar(&num, "n", 1, "number of wallpapers that you want from Bing :)")
+
 	usr, err := user.Current()
 	if err != nil {
 		glog.Errorf("OS.User: %v", err)
 	}
 
-	bing.GetBingDesktop(fmt.Sprintf("%s/Pictures/Bing", usr.HomeDir), true)
+	bing.GetBingDesktop(fmt.Sprintf("%s/Pictures/Bing", usr.HomeDir), false, num-1, num)
 }
