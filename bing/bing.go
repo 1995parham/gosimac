fe5a4e0481e51ba840b31b52f594110ae6ab0c23
Parham Alvani <parham.alvani@gmail.com>
2018-08-30 04:24:09 +0430

    Convert bing to source.

--- bing/bing.go
+++ bing/bing.go
@@ -6,84 +6,59 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
-	"sync"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
-var w sync.WaitGroup
+// Source is source implmentation for bing everyday image
+type Source struct {
+	response Response
+	Idx      int
+	N        int
+}
 
-func getBingImage(path string, image Image) {
-	log.Infof("Getting %s\n", image.StartDate)
-
-	defer w.Done()
-
-	if _, err := os.Stat(fmt.Sprintf("%s/%s.jpg", path, image.FullStartDate)); err == nil {
-		log.Infof("%s is already exists\n", image.StartDate)
-		return
+// Init initiates source and return number of avaiable images
+func (s *Source) Init() (int, error) {
+	// Create HTTP GET request
+	resp, err := http.Get(
+		fmt.Sprintf("http://www.bing.com/HPImageArchive.aspx?format=js&idx=%d&n=%d&mkt=en-US",
+			s.Idx, s.N))
+	if err != nil {
+		return 0, fmt.Errorf("network failure on %s: %v", "http://www.bing.com/hpimagearchive.aspx", err)
 	}
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("Invalid response: %s", resp.Status)
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&s.response); err != nil {
+		return 0, fmt.Errorf("decoding json: %v", err)
+	}
+
+	if err := resp.Body.Close(); err != nil {
+		return 0, fmt.Errorf("(io.Closer).Close: %v", err)
+	}
+
+	return len(s.response.Images), nil
+}
+
+// Name returns source name
+func (s *Source) Name() string {
+	return "bing"
+}
+
+// Fetch fetches given index from source
+func (s *Source) Fetch(index int) (string, io.ReadCloser, error) {
+	image := s.response.Images[index]
+
+	logrus.Infof("Getting %s\n", image.StartDate)
 
 	resp, err := http.Get(fmt.Sprintf("http://www.bing.com/%s", image.URL))
 	if err != nil {
-		log.Errorf("http.Get: %v", err)
-		return
+		return "", nil, err
 	}
 
-	defer func() {
-		if err := resp.Body.Close(); err != nil {
-			log.Errorf("(io.Closer).Close: %v", err)
-		}
-	}()
+	logrus.Infof("%s was gotten\n", image.StartDate)
 
-	destFile, err := os.Create(fmt.Sprintf("%s/%s.jpg", path, image.FullStartDate))
-	if err != nil {
-		log.Errorf("os.Create: %v", err)
-		return
-	}
-
-	defer func() {
-		if err := destFile.Close(); err != nil {
-			log.Errorf("(*os.File).Close: %v", err)
-		}
-	}()
-
-	if _, err := io.Copy(destFile, resp.Body); err != nil {
-		log.Errorf("io.Copy: %v", err)
-	}
-
-	log.Infof("%s was gotten\n", image.StartDate)
-}
-
-// GetBingDesktop function gets `n` Bing Wallpaper since `idx` and stores them in `path`.
-func GetBingDesktop(path string, idx int, n int) error {
-	// Create HTTP GET request
-	resp, err := http.Get(
-		fmt.Sprintf("http://www.bing.com/hpimagearchive.aspx?format=js&index=%d&number=%d&mkt=en-US",
-			idx, n))
-	if err != nil {
-		return fmt.Errorf("network failure on %s: %v", "http://www.bing.com/hpimagearchive.aspx", err)
-	}
-
-	defer func() {
-		if err := resp.Body.Close(); err != nil {
-			log.Errorf("(io.Closer).Close: %v", err)
-		}
-	}()
-
-	var bingResp Response
-	if err := json.NewDecoder(resp.Body).Decode(&bingResp); err != nil {
-		return fmt.Errorf("decoding json: %v", err)
-	}
-
-	// Create spreate thread for each image
-	for _, image := range bingResp.Images {
-		w.Add(1)
-		go getBingImage(path, image)
-	}
-
-	// Waiting for getting all the images
-	w.Wait()
-
-	return nil
+	return fmt.Sprintf("%s.jpg", image.FullStartDate), resp.Body, nil
 }
