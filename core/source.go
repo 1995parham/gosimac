34cb9da6b0c1248ddc3225a84f23658a764fef71
Parham Alvani <parham.alvani@gmail.com>
2019-10-08 20:26:02 +0330

    fix: Correct a Typo

--- core/source.go
+++ core/source.go
@@ -17,7 +17,7 @@ import "io"
 
 // Source represents source for image background
 type Source interface {
-	Init() (int, error)                             // call once on source and return number of avaiable images to fetch
+	Init() (int, error)                             // call once on source and return number of available images to fetch
 	Name() string                                   // name of source in string format
 	Fetch(index int) (string, io.ReadCloser, error) // fetch image from source
 }
