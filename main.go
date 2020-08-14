f15308e22b9990e99b5d850e49e80d49f6a84017
Parham Alvani <parham.alvani@gmail.com>
2020-08-15 00:25:05 +0430

    feat: Add Version Information

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/1995parham/gosimac/cmd"
+import (
+	"fmt"
+
+	"github.com/1995parham/gosimac/cmd"
+)
+
+// nolint: gocheckglobals
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
 
 func main() {
+	fmt.Printf("gosimac %s, commit %s, built at %s\n", version, commit, date)
+
 	cmd.Execute()
 }
